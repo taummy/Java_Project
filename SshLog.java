ea6c66cd9f5e5a9cfc77cc3da810f7a42a6aeb0d
syafieqpitt <qhz.syafieq@gmail.com>
2019-03-16 21:12:37 +0100

    Update SshLog.java

--- SshLog.java
+++ SshLog.java
@@ -1,32 +1,42 @@
 /**
  * @author irfan
- * @author tom
  */
+package server;
 import java.util.*;
 import java.io.*;
 import java.text.*;
 
 public class SshLog extends Logs{
 	
-	public static String laDate; //la date
-	public static String leTemps; // le temps d'execution
-	public static String nomServer; //le nom du serveur
-	public static String numeroPort; //le numero du port
-	public static String typeConnection; 
-	public static String session;
-	public static String lineRead;
-	public static String line;
-	public static String[] chaine1, chaine2;
-	public static String[] splitLine, tabC1;
+	private String laDate; //la date
+	private String leTemps; // le temps d'execution
+	private String nomServer; //le nom du serveur
+	private String typeConnection; 
+	private String session;
 	
-public static void sshProcess(String file) {
+	private String lineRead;
+	private String line;
+	private String[] chaine1, chaine2;
+	private String[] splitLine, tabC1;
+	
+	public SshLog() {
+		laDate="";
+		leTemps="";
+		nomServer="";
+		typeConnection="";
+		session="";
+		
+	}
+	
+//La fonction pour extraire les informations du fichier
+public void sshProcess(String file) {
 		
 		try(BufferedReader reader = new BufferedReader(new FileReader(file))){
 			lineRead=reader.readLine();
 			while(lineRead !=null) {
 				splitLine=lineRead.split("]: ");
 				chaine1= splitLine[0].split("\\s+");
-				laDate=chaine1[0] + chaine1[1];
+				laDate=chaine1[1] +" "+chaine1[0];
 				leTemps=chaine1[2];
 				nomServer=chaine1[3];
 				typeConnection=chaine1[4]+"]";
@@ -44,9 +54,11 @@ public static void sshProcess(String file) {
         }
 	}
 	
-	//testons notre methode sshProcess
+	//testons notre methode apacheProcess
 	public static void main(String[] args) {
-		sshProcess("exemple.linux.auth.sshd.log.txt");
+		SshLog s1;
+		s1 = new SshLog();
+		s1.sshProcess("exemple.linux.auth.sshd.log.txt");
 	}
 
 }
