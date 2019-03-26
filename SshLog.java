04d5455bdf11ac5a36fa5e6ddc493db5a07acf5a
syafieqpitt <qhz.syafieq@gmail.com>
2019-03-26 22:55:37 +0100

    Update SshLog.java

--- SshLog.java
+++ SshLog.java
@@ -15,21 +15,23 @@ import java.text.*;
 
 public class SshLog extends Logs{
 	
-	public String laDate; //la date
-	public String leTemps; // le temps d'execution
-	public String nomServer; //le nom du serveur
-	public String numeroPort; //le numero du port
-	public String typeConnection; 
-	public String session;
+	public String laDate; //Date 
+	public String leTemps; //Running time
+	public String nomServer; //Server name
+	public String typeConnection; //Type of connection sshd["port number"]
+	public String session; //Activity while connected to server
 	public String lineRead;
 	public String line;
 	public String[] chaine1, chaine2;
 	public String[] splitLine, tabC1;
+	
+	//Creation of variable of class Princiaple
         public Principale princ;
         public SshLog(Principale p){
             princ=p;
         }
-        
+	
+        //Constructor by default
         public SshLog(){
             laDate="";
             leTemps="";
@@ -40,6 +42,8 @@ public class SshLog extends Logs{
             lineRead="";
             line="";
         }
+	
+	//Constructor with factors
         public SshLog(String laDate,String leTemps,String nomServer, String typeConnection,String session){
             this.laDate=laDate;
             this.leTemps=leTemps;
@@ -47,6 +51,8 @@ public class SshLog extends Logs{
             this.typeConnection=typeConnection;
             this.session=session;
         }
+	
+	//Reading accesors
         public String getDate(){
             return laDate;
         }
@@ -64,33 +70,34 @@ public class SshLog extends Logs{
         }
         
         
-	
+//Function for data parsing and the name of file as factor of the function
 public void sshProcess(String file) {
-		
+	
+		//use BufferedReader to read file
 		try(BufferedReader reader = new BufferedReader(new FileReader(file))){
 			lineRead=reader.readLine();
 			while(lineRead !=null) {
-				splitLine=lineRead.split("]: ");
-				chaine1= splitLine[0].split("\\s+");
-				laDate=chaine1[0] + chaine1[1];
-				leTemps=chaine1[2];
-				nomServer=chaine1[3];
-				typeConnection=chaine1[4]+"]";
-				session=splitLine[1];
+				splitLine=lineRead.split("]: "); //split the line into two parts, for example: "Apr 13 06:47:17 kheops sshd[4866]: " and  "reverse mapping checking getaddrinfo for n37-137.berlin.snafu.de [195.21.37.137] failed - POSSIBLE BREAK-IN ATTEMPT!"
+				chaine1= splitLine[0].split("\\s+"); //split 1st part of the line 
+				laDate=chaine1[0] + chaine1[1]; //Extraction of the date of connection
+				leTemps=chaine1[2];		//Running time
+				nomServer=chaine1[3];		//Server name
+				typeConnection=chaine1[4]+"]";	//Type of connection with port number
+				session=splitLine[1];		//Activity while connection
 				
-				//Ajout dans la table sshLog
+				//Called a function in Principale.java using an object class Principale. Take all the data in variables as factors in insertSsh.
 				princ.insertSsh(laDate, leTemps, nomServer, typeConnection, session);
 				lineRead= reader.readLine();
 			}
 			
 		}
-		
+		//show error message
 		catch(IOException e){
             System.out.println(e.getMessage());
         }
 	}
 	
-	//testons notre methode sshProcess
+	//Try  sshProcess
 	public static void main(String[] args) {
             Principale p;
         p= new Principale();
